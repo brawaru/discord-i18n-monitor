462c2e78737d9a979a100f89bcc17cec845b16b8
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2019-08-22 20:06:45 +0700

    :sparkles: Add automatic extractor

---
+++ commiter.js
@@ -0,0 +1,158 @@
+const { spawn } = require("child-process-promise");
+
+/**
+ * Gets current branch name
+ * @returns {string} Current branch name
+ */
+function getCurrentBranch() {
+    return new Promise(async (resolve) => {
+        const { childProcess } = spawn("git", ["rev-parse", "--abbrev-ref", "HEAD"]);
+
+        childProcess.stdout.on("data", (data) => resolve(data.toString().trim()));
+    });
+}
+
+/**
+ * Parses revision
+ * @param {string} revName Object name to parse
+ * @returns {string} Parsed revision?
+ */
+function parseRev(revName) {
+    return new Promise(async (resolve) => {
+        const { childProcess } = spawn("git", ["rev-parse", "--quiet", "--verify", revName]);
+
+        childProcess.stdout.on("data", (data) => resolve(data.toString().trim()));
+    });
+}
+
+/**
+ * Checks out to the branch
+ * @param {string} branchName Branch name
+ */
+async function checkout(branchName) {
+    // It has no sence to checkout onto branch we're in
+
+    const currentBranch = await getCurrentBranch();
+
+    if (currentBranch !== branchName) {
+        console.log("Checking out...");
+
+        const args = ["checkout", branchName];
+
+        { // If branch does not exist we need to create it
+            const revision = await parseRev(branchName);
+
+            if (revision === null) args.splice(1, 0, "-b");
+        }
+
+        let checkedOut = true;
+
+        {
+            const checkingOut = spawn(`git`, args).then((result) => {
+                console.info(`Checked out to "${branchName}"!`);
+            });
+
+            let errOutput = "";
+
+            checkingOut.childProcess.stderr.on("data", (data) => errOutput += data);
+
+            checkingOut.catch(() => {
+                console.error(errOutput);
+
+                checkedOut = false;
+            });
+
+            await checkingOut;
+        }
+
+        if (!checkedOut) throw new Error("Checkout failed");
+    }
+}
+
+/**
+ * Creates a commit
+ * @param {string} message Commit message
+ * @returns {boolean} Whether commit was created or not
+ */
+async function createCommit(message) {
+    const commit = spawn(`git`, ["commit", "-m", message]);
+
+    let output = "";
+    let errOutput = "";
+
+    commit.childProcess.stdout.on("data", (data) => output += data);
+    commit.childProcess.stderr.on("data", (data) => errOutput += data);
+
+    let safeError = true;
+
+    commit.catch(() => {
+        if (output.includes("no changes added to commit")) {
+            return console.log("  There were no changes. Commit creation skipped!");
+        }
+
+        console.error(`Commit creation failed.\n${output}`);
+
+        safeError = false;
+    });
+
+    try {
+        await commit;
+
+        return true;
+    } catch (err) {
+        // Some errors are safe to skip, such as when there were no changes...
+        if (!safeError) {
+            console.log(output);
+            console.error(errOutput);
+
+            throw err;
+        }
+
+        return false;
+    }
+}
+
+async function addFile(fileName) {
+    await spawn(`git`, ["add", fileName]);
+}
+
+async function runCommiter(files) {
+    const date = new Date();
+
+    const branchName = `update/${date.getDate() + 1}.${date.getMonth() + 1}.${date.getFullYear() - 2000}`;
+
+    await checkout(branchName);
+
+    const currentDate = new Intl.DateTimeFormat('ru-RU', {
+        year: '2-digit',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+    }).format(date);
+
+    let changes = false;
+
+    for (let i = 0, l = files.length; i < l; i++) {
+        const [code, fileName] = files[i];
+
+        console.log(`Commiting ${code} (${fileName})...`);
+
+        await addFile(fileName);
+
+        console.log(`  File ${fileName} added. Is it?`)
+
+        const commitCreated = await createCommit(`:rocket: Revision "${code}" as of ${currentDate}`);
+
+        if (commitCreated) {
+            console.log("  Commit created");
+
+            changes = true;
+        }
+    }
+
+    if (changes) console.log("There were commits created. Don't forget to push!");
+}
+
+module.exports = runCommiter;
---
+++ index.js
@@ -0,0 +1,207 @@
+// Initializing Puppeter
+
+const puppeter = require("puppeteer");
+
+const { promises: fs } = require("fs");
+
+const configuration = {
+    branch: process.env["DISCORD_BRANCH"] || "canary",
+    context: process.env["DISCORD_CONTEXT"] || "client", 
+    autoCommit: process.env["DISCORD_AUTOCOMMIT"] === "1" ? true : false
+};
+
+(async () => {
+    console.log("Initializing browser view");
+
+    const browser = await puppeter.launch();
+
+    const tab = await browser.newPage();
+
+    console.log("Navigating to Discord application page");
+
+    {
+        let url = configuration.branch === "stable"
+            ? "https://discordapp.com/"
+            : `https://${configuration.branch}.discordapp.com/`;
+
+        if (configuration.context === "client") url += "login";
+
+        await tab.goto(url, { waitUntil: "networkidle2" });
+    }
+
+    console.log("Intruding into Webpack");
+
+    const intruderPushed = await tab.evaluate(() => {
+        let localesModule = window.__localesModule;
+
+        if (localesModule === undefined) {
+            let webpackModules = window.__webpackModules;
+    
+            if (webpackModules === undefined) {
+                console.log("[DDI] Intruding and exposing Webpack modules...");
+    
+                webpackModules = (() => {
+                    // Intruder pushes itself to the Webpack chunks to get inside context
+                    const moduleName = 'DNIntruder';
+    
+                    let webpackGetModule;
+    
+                    window.webpackJsonp.push([
+                        [], {
+                            [moduleName]: (module, _, getModule) => {
+                                // Intruder's code, it should expose us getModule function
+    
+                                webpackGetModule = getModule;
+    
+                                module.exports = {};
+                            }
+                        },
+                        [[moduleName]]
+                    ]);
+    
+                    return webpackGetModule.c;
+                })();
+    
+                Object.defineProperty(window, "__webpackModules", {
+                    value: webpackModules
+                });
+            }
+
+            function findChunk(predicate) {
+                for (const key in webpackModules) {
+                    const exported = webpackModules[key].exports;
+    
+                    if (exported.__esModule && exported.default && predicate(exported.default)) {
+                        return exported.default;
+                    }
+    
+                    if (exported && predicate(exported)) return exported;
+                }
+            }
+    
+            console.log("[DDI] Attempting to find localization module...")
+    
+            localesModule = findChunk(
+                (obj) => obj.getLanguages != null && obj._getMessages != null
+            );
+    
+            if (localesModule == null) {
+                console.error("[DDI] Unable to find loaded localization module - are we using Discord?");
+    
+                return false;
+            }
+    
+            Object.defineProperty(window, "__localesModule", {
+                value: localesModule
+            });
+
+            return true;
+        }
+    });
+
+    if (!intruderPushed) {
+        console.error("Unable to expose localization module, was Discord updated or is not available?");
+
+        throw new Error("Failed to set intruder in place");
+    }
+
+    console.log("Intruder in place, commanding it to query list of locales...");
+
+    const locales = await tab.evaluate(() => window.__localesModule.getLanguages());
+
+    const localesCount = locales.length;
+
+    console.log(`${localesCount} locales available. Extraction...`);
+
+    const saveContext = configuration.context === "site" ? "site" : "client";
+
+    let files = [];
+
+    for (let i = 0; i < localesCount; i++) {
+        const { code, englishName, enabled } = locales[i];
+
+        console.log(`Extracting ${code} (${englishName})`);
+
+        const id = `isr${Math.floor(Math.random() * 100000)}`;
+
+        if (!enabled) {
+            console.log("  Is disabled - skipping");
+
+            continue;
+        }
+
+        console.log("  Commanding to extract");
+
+        await tab.evaluate(
+            `(async () => {
+    let readyState = [false];
+
+    Object.defineProperty(window, "${id}", {
+        value: () => {
+            if (readyState[0]) delete window["${id}"];
+
+            return readyState;
+        },
+        configurable: true
+    });
+
+    try {
+        const locale = await window.__localesModule._getMessages("${code}");
+
+        readyState = [true, JSON.stringify(locale, null, "\t")];
+    } catch (err) {
+        readyState = [true, 0];
+    }
+})()`
+        );
+
+        console.log("  Awaiting for command to complete...");
+
+        let commandResult = await (new Promise((res, rej) => {
+            let iterationsLeft = 100;
+
+            let iterationInterval = setInterval(async () => {
+                if (--iterationsLeft === -1) {
+                    clearInterval(iterationInterval);
+
+                    return rej("Timed out");
+                }
+
+                const commandQuery = await tab.evaluate(`window.${id}()`);
+
+                if (commandQuery[0]) {
+                    clearInterval(iterationInterval);
+
+                    return res(commandQuery[1]);
+                }
+
+            }, 50);
+        }));
+
+        const pathPrefix = `./locales/${saveContext}/`;
+
+        try {
+            await fs.mkdir(pathPrefix, { recursive: true });
+        } catch (err) {
+            console.warn(err);
+        }
+
+        if (commandResult !== 0) {
+            console.log("  Complete. Saving...");
+
+            const fileName = `${pathPrefix}/${code}.json`;
+
+            await fs.writeFile(fileName, commandResult, { encoding: "utf8" });
+
+            files.push([code, fileName]);
+        } else {
+            console.warn("  Cannot get locale file - probably Discord's fault");
+        }
+    }
+
+    if (configuration.autoCommit) await (require("./commiter"))(files);
+
+    console.log("Complete. Bye");
+
+    process.exit(0);
+})();
